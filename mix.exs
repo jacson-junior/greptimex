67c938de38d21d83d57990bffa0203af59ca2da8
Jacson Junior <jacson@sintax.dev>
2025-12-14 13:40:52 -0300

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Greptimex.MixProject do
   def project do
     [
       app: :greptimex,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       description: description(),
